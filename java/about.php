2f60e3e5b71a5aadef5483b59bfc37cf60c65c75
McPaSaHa <95250059+McPaSaHa@users.noreply.github.com>
2023-11-22 12:46:09 +0200

    Add files via upload

---
+++ java/about.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <title>Ваше портфоліо</title>
+</head>
+<body>
+    <!-- Навігаційна панель Bootstrap -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Моє портфоліо</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Домашня</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Про мене</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Проекти</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Контакти</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <div class="card">
+                    <img src="img\ava.png" class="card-img-top mx-auto d-block img-fluid" alt="Ваше зображення" style="width: 300px; height: 300px;"><div class="card-body">
+                        <h5 class="card-title">Pasha</h5>
+                        <p class="card-text">Студент ЧНУ!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+<footer class="bg-dark text-white text-center py-3">
+        <p>&copy; 2023 Pasha. Усі права захищені.</p>
+    </footer>
+</html>
---
+++ java/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <title>Ваше портфоліо</title>
+</head>
+<body>
+    <!-- Навігаційна панель Bootstrap -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Моє портфоліо</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Домашня</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.php">Про мене</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Проекти</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Контакти</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img\istockphoto-1140180560-612x612.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img\images1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img\images.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</body>
+
+<footer class="bg-dark text-white text-center py-3">
+        <p>&copy; 2023 Pasha. Усі права захищені.</p>
+    </footer>
+</html>
